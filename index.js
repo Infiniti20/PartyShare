a7d730248a87704a490f8318f4a635396bdd4650
Infiniti20 <>
2021-05-25 20:43:16 +0000

    Image hosting setup is done.

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ var cookieParser = require('cookie-parser')
 
 //Libraries
 const multer=require('multer')
-const upload=multer({storage: multer.memoryStorage()})
 var admin = require("firebase-admin");
 
 
@@ -28,15 +27,32 @@ app.options('*', cors());
 var body = bodyParser.json()
 app.use('/views', express.static('views'));
 
+//Firebase Storage Setup
+admin.initializeApp({
+  credential: admin.credential.cert(JSON.parse(process.env.JSON)),
+  storageBucket: process.env.BUCKET_URL
+});
+
 //c4eeb4f2-1912-484af-9e5-1 product Id
 
 //Variables
 const cache=new caching.Cache()
+const upload=multer({storage: multer.memoryStorage(),fileFilter:utils.filter})
+app.locals.bucket = admin.storage().bucket()
+
 
 //HTML Routes
 app.get("/",(req,res)=>{
  let account=req.cookies.token||undefined;
- res.send("Unfinished")
+ res.send(`<form method="POST" action="/api/products/new" enctype="multipart/form-data">
+    <div>
+        <label>Select your profile picture:</label>
+        <input type="file" name="profile_pic" />
+    </div>
+    <div>
+        <input type="submit" name="btn_upload_profile_pic" value="Upload" />
+    </div>
+</form>`)
 })
 
 //API Routes
@@ -51,10 +67,18 @@ app.get("/api/products/:id/",(req,res)=>{
 app.post("/api/products/:id/update/details",body,(req,res)=>{
 	db.prepare("UPDATE products SET name = ?, desc = ?, image = ? WHERE uuid = ?").run(req.body.name, req.body.desc, req.body.image, req.params.id)
 })
+
 app.post("/api/products/:id/update/pricing",body,(req,res)=>{
 	db.prepare("UPDATE products SET cost = ?, quantity = ? WHERE uuid = ?").run(req.body.cost, req.body.quantity, req.params.id)
 })
-app.post("/api/products/new",body,(req,res)=>{})
+
+app.post("/api/products/new",body,upload.single('profile_pic'),async (req,res)=>{
+	let ext=utils.getExt(req.file.originalname)
+	let name=utils.computeHash(req.file.originalname+Math.random())+"."+ext
+	//Insert into DB next
+	await app.locals.bucket.file(name).createWriteStream().end(req.file.buffer)
+	res.json({status:"200 OK"})
+})
 
 
 app.listen(3000,()=>{console.log("Server running...")})
--- utils.js
+++ utils.js
@@ -1,15 +1,27 @@
 const crypto = require('crypto');
 
-module.exports={
- computeHash:function (text){
-  const hash = crypto.createHash('sha256').update(text).digest('base64');
-  return hash
- },
- generateUUID:function() {
-  return 'xxxxxxxx-xxxx-4xxxx-xxx-x'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0,
-      v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
- }
+module.exports = {
+	computeHash: function (text) {
+		const hash = crypto.createHash('sha256').update(text).digest('base64');
+		return hash
+	},
+	generateUUID: function () {
+		return 'xxxxxxxx-xxxx-4xxxx-xxx-x'.replace(/[xy]/g, function (c) {
+			var r = Math.random() * 16 | 0,
+				v = c == 'x' ? r : (r & 0x3 | 0x8);
+			return v.toString(16);
+		});
+	},
+	filter: function (req, file, cb) {
+		// Accept images only
+		if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG)$/)) {
+			req.fileValidationError = 'Only image files are allowed';
+			return cb(new Error('Only image files are allowed'), false);
+		}
+		cb(null, true);
+	},
+	getExt: function (filename) {
+		var i = filename.lastIndexOf('.');
+		return (i < 0) ? '' : filename.substr(i);
+	}
 }
